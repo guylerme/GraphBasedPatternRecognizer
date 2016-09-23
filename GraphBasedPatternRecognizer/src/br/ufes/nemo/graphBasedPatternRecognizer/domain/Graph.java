a7069ba6a732479760c2ff24c86cfd1bd3b3f1a8
Guylerme <guylerme@gmail.com>
2016-09-23 11:23:29 -0300

    Graph domain definition

---
+++ GraphBasedPatternRecognizer/src/br/ufes/nemo/graphBasedPatternRecognizer/domain/Graph.java
@@ -0,0 +1,44 @@
+package br.ufes.nemo.graphBasedPatternRecognizer.domain;
+
+import java.util.Set;
+
+public class Graph {
+
+	private String id;
+	private Set<Graph> subGraphs;
+	private Set<Node> nodes;
+	private Set<Edge> edges;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Set<Graph> getSubGraphs() {
+		return subGraphs;
+	}
+
+	public void setSubGraphs(Set<Graph> subGraphs) {
+		this.subGraphs = subGraphs;
+	}
+
+	public Set<Node> getNodes() {
+		return nodes;
+	}
+
+	public void setNodes(Set<Node> nodes) {
+		this.nodes = nodes;
+	}
+
+	public Set<Edge> getEdges() {
+		return edges;
+	}
+
+	public void setEdges(Set<Edge> edges) {
+		this.edges = edges;
+	}
+
+}
