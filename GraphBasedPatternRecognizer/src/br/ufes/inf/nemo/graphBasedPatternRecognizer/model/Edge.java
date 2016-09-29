c5e8d3c05071047b01c39f4a9bea9185103a165d
Guylerme <guylerme@gmail.com>
2016-09-29 09:38:50 -0300

    Comitando a mudanca de nome de pacote

---
+++ GraphBasedPatternRecognizer/src/br/ufes/inf/nemo/graphBasedPatternRecognizer/model/Edge.java
@@ -0,0 +1,32 @@
+package br.ufes.inf.nemo.graphBasedPatternRecognizer.model;
+
+public class Edge {
+	private String label;
+	private Node origin;
+	private Node destination;
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Node getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(Node origin) {
+		this.origin = origin;
+	}
+
+	public Node getDestination() {
+		return destination;
+	}
+
+	public void setDestination(Node destination) {
+		this.destination = destination;
+	}
+
+}
